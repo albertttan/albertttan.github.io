bb0175834f0ffb2161c9188adf8f3a062ddb82a6
Albert Tan <albert-tan@qq.com>
2025-03-24 12:32:14 +0000

    Portfolio memory fragment

---
+++ portfolio/fragment.js
@@ -0,0 +1,19 @@
+const images = ['1.jpeg', '2.jpeg', '3.jpeg'];
+
+function shuffle() {
+    const img = new Image();
+    img.src = `/portfolio/photos/static/${images[Math.floor(Math.random() * images.length)]}`;
+    img.onload = () => {
+        const x = Math.random() * (img.width - 16), y = Math.random() * (img.height - 16);
+        const canvas = document.getElementById('canvas');
+        const ctx = canvas.getContext('2d');
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(img, x, y, 16, 16, 0, 0, canvas.width, canvas.height);
+        document.getElementById('original-link').href = img.src;
+    };
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('shuffle').addEventListener('click', shuffle);
+    shuffle();
+});
